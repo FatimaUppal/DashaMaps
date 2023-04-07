098e0cb1b1d6fbdd1277eec4614b225007acc192
FatimaUppal <125939989+FatimaUppal@users.noreply.github.com>
2023-04-07 17:46:59 -0400

    finished size, set and bucket size

--- src/main/java/com/github/zipcodewilmington/DashaMap.java
+++ src/main/java/com/github/zipcodewilmington/DashaMap.java
@@ -9,9 +9,24 @@ import java.beans.PropertyEditorSupport;
  */
 public class DashaMap implements HashMapX {
     Node [] arr= new Node[26];
+    private int HashFunctionOne(String input) {
+        if (input.length() > 0) {
+            return input.toLowerCase().charAt(0) -97;
+        }
+        return -1;
+    }
 
     @Override
     public void set(String key, String value) {
+        int index= HashFunctionOne(key);
+        Node current=arr[index];
+        if(current== null){
+            current= new Node(key, value);
+        }
+        while(current.getNext() != null){
+            current = current.getNext();
+        }
+        current.setNext(new Node(key, value));
     }
 
     @Override
@@ -28,16 +43,30 @@ public class DashaMap implements HashMapX {
     }
     @Override
     public long size(){
-        return 0;
+        int count=0;
+        for (int i = 0; i < arr.length; i++) {
+            Node current= arr[i];
+            while(current != null){
+                count++;
+                current=current.getNext();
+            }
+        }
+        return count;
     }
 
     @Override
     public long bucketSize(String key) {
-        return 0;
+        long count= 0;
+        Node current= arr[HashFunctionOne(key)];
+        while(current != null){
+            count++;
+            current=current.getNext();
+        }
+        return count;
     }
 
 
-    public long getBucket(String k) {
-        return 0;
+    public long getBucket(String key) {
+
     }
 }
